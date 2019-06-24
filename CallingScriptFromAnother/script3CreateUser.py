b615c5ab45342d3d4561da850f182724e54dc3e4
vnigam <vnigam1985@gmail.com>
2019-06-24 03:36:33 +0000

    Fix argument error

--- CallingScriptFromAnother/script3CreateUser.py
+++ CallingScriptFromAnother/script3CreateUser.py
@@ -1,9 +1,9 @@
 import boto3
 import sys
-def CreateUser():
+def CreateUser(user):
 	iam= boto3.client('iam')
-	iam.create_user( UserName="avengers")
-	#iam.attach_user_policy(UserName = 'avengers', PolicyArn='arn:aws:iam::aws:policy/AmazonEC2FullAccess')
+	iam.create_user(UserName=user)
+	iam.attach_user_policy(UserName = user, PolicyArn='arn:aws:iam::aws:policy/AmazonEC2FullAccess')
 	print("SUCCESS ")
 	#iam.detach_role_policy(PolicyArn='arn:aws:iam::aws:policy/AmazonEC2FullAccess',RoleName='AmazonEC2FullAccess')
-	iam.delete_user(UserName="avengers")
+	#iam.delete_user(UserName="avengers")
