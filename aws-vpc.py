3e1d7659f31a964702196081d56f574b11b47225
Vishal Lathar <34193553+vlathar@users.noreply.github.com>
2019-07-16 15:36:04 +0530

    Update aws-vpc.py

--- aws-vpc.py
+++ aws-vpc.py
@@ -1,5 +1,5 @@
 import boto3
-ec2 = boto3.client("ec2")
+ec2 = boto3.client("ec2",region_name='us-west-2')
 
 #create vpc
 vpc = ec2.create_vpc(CidrBlock='10.0.0.0/16',DryRun=False)
@@ -15,7 +15,7 @@ print(subnet1["Subnet"]["SubnetId"])
 subnet2=ec2.create_subnet(CidrBlock='10.0.2.0/24',VpcId=vpc_id)
 print(subnet2["Subnet"]["SubnetId"])
 
-subnet3=ec2.create_subnet(CidrBlock='10.0.3.0/24',VpcId=vpc._id)
+subnet3=ec2.create_subnet(CidrBlock='10.0.3.0/24',VpcId=vpc_id)
 print(subnet3["Subnet"]["SubnetId"])
 
 ec2.create_tags(Resources=[subnet1["Subnet"]["SubnetId"]],Tags=[{'Key':'Name','Value':'public_subnet_1'}])
@@ -28,7 +28,7 @@ ec2.attach_internet_gateway(VpcId=vpc_id,InternetGatewayId=ig["InternetGateway"]
 print("Internet gateway created and attached to vpc.")
 
 #creating route table
-rt=vpc.create_route_table()
+rt=ec2.create_route_table(VpcId=vpc_id)
 ec2.create_route(RouteTableId=rt["RouteTable"]["RouteTableId"],DestinationCidrBlock='0.0.0.0/0',GatewayId=ig["InternetGateway"]["InternetGatewayId"])
 print("Route table created and route added.")
 
